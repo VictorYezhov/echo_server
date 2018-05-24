d29131ac093e5f8ac810fc399690a12dabad571f
VictorYezhov <vicyezh@gmail.com>
2018-05-24 16:38:10 +0300

    some succsessfull stage

--- main.cpp
+++ main.cpp
@@ -14,6 +14,9 @@
 #include <boost/thread.hpp>
 #include <boost/format.hpp>
 #include <boost/smart_ptr.hpp>
+#include <deque>
+
+std::mutex mutex;
 
 using boost::asio::ip::tcp;
 using boost::system::error_code;
@@ -22,18 +25,20 @@ using namespace boost::posix_time;
 class client_session
 {
 public:
-    typedef boost::shared_ptr<client_session> pointer;
+    bool ready;
 
-    static pointer create(boost::asio::io_service &io) {
-        return pointer(new client_session(io));
+    static client_session* create(boost::asio::io_service &io) {
+        return new client_session(io);
     }
 
     ~client_session() {
         //log("Connection closed");
-        socket_.close();
+        socket_->close();
+        ready = false;
+        std::cout<<"client deleted\n";
     }
 
-    tcp::socket &socket() { return socket_; }
+    boost::shared_ptr<tcp::socket> socket() { return socket_; }
 
     size_t read_complete(char * buff, const error_code & err, size_t bytes) {
         if ( err) return 0;
@@ -42,21 +47,20 @@ public:
     }
 
 
-    void start() {
-        //log("Connection established");
+    void recreate(){
+        socket_.reset(new tcp::socket(io));
+       // log("socket recreated");
+    }
 
-        char buff[1024];
-
-        socket_.read_some(buffer(buff));
-
-        socket_.write_some(buffer(buff));
-
-
+    void delete_sock(){
+        socket_.reset();
+       // log("socket deleted");
     }
 
 protected:
-    client_session(boost::asio::io_service &io)
-            : socket_(io) {
+    client_session(boost::asio::io_service &io):io(io){
+        socket_.reset(new tcp::socket(io));
+        ready = true;
     }
 
     std::string make_time_string() {
@@ -67,25 +71,50 @@ protected:
 
     void log(std::string const &message) {
         std::clog << boost::format("%|-25| [client address: %|15|]\n")
-                     % message % socket_.remote_endpoint().address().to_string();
+                     % message % socket_->remote_endpoint().address().to_string();
     }
 
 private:
-    tcp::socket socket_;
+
+    boost::asio::io_service &io;
+    boost::shared_ptr<tcp::socket> socket_;
 };
 
+void start_work(client_session &client) {
+
+
+    char buff[1024];
+    client.socket()->read_some(buffer(buff));
+    client.socket()->write_some(buffer(buff));
+    client.socket()->close();
+    client.delete_sock();
+    //client.recreate();
+}
+
+
+
+std::vector<client_session*> available_sessions;
 int main()
 {
     boost::asio::io_service io;
+    for(int i=0; i< 50001; i++){
+        available_sessions.push_back(client_session::create(io));
+    }
+    std::cout<<"Server ready"<<std::endl;
 
     tcp::acceptor acceptor(io, tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 8001));
+    int i =0;
     for (;;) {
         try {
-            client_session::pointer new_client = client_session::create(io);
-            acceptor.accept(new_client->socket());
-            boost::thread(boost::bind(&client_session::start, new_client)).detach();
-        }catch (...){
+            acceptor.accept(*(available_sessions[i]->socket()));
+            std::thread t4(start_work, std::ref(*available_sessions[i]));
+            i++;
+            t4.detach();
+            //if(i == 999){
+              //  i = 0;
+            //}
 
+        }catch (...){
         }
     }
 }
